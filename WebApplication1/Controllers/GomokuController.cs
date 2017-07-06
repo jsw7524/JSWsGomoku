05fb5c60d27cbfe9525d53ed3914204eb6675a4f
Jason Wu <jsw7524@gmail.com>
2017-07-06 21:30:54 +0800

    Add project files.

---
+++ WebApplication1/Controllers/GomokuController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Helper;
+
+namespace WebApplication1.Controllers
+{
+    public class GomokuController : Controller
+    {
+        // GET: Gomoku
+        Random rand = new Random();
+        Rule rule = new Rule();
+        AI myAI = new AI();
+
+        private int roundCount = 0;
+        public ActionResult Play()
+        {
+            return View();
+        }
+
+        public ActionResult AI(int posY, int posX,int side=-1)
+        {
+            if (posY < 0 || posY > 14 || posX < 0 || posX > 14)
+            {
+                return new EmptyResult();
+            }
+
+            rule.myTable = (int[,])TempData["myTable"] ?? new int[15, 15];
+            roundCount = (int?) TempData["roundCount"] ?? 0;
+
+            //Player Part
+            rule.myTable[posY, posX] = side;
+            roundCount++;
+
+            if (rule.Referee(posY, posX, side))
+            {
+                rule.ResetGame();
+                TempData["myTable"] = rule.myTable;
+                TempData["roundCount"] = 0;
+                return Json(new { yAI = posY, xAI = posX, sideAI = (side == 1) ? -1 : 1, flag=99,message="Player wins." }, JsonRequestBehavior.AllowGet); // Player wins the game.
+            }
+            //
+
+            //AI Part
+            if (1 == roundCount)
+            {
+                rule.myTable[posY, posX + 1] = (side == 1) ? -1 : 1;
+                TempData["myTable"] = rule.myTable;
+                roundCount++;
+                TempData["roundCount"] = roundCount;
+                return Json(new { yAI = posY, xAI = posX+1, sideAI = (side == 1) ? -1 : 1, flag = 0, message = "" }, JsonRequestBehavior.AllowGet); 
+            }
+            myAI.LoadWeightTable();
+            myAI.myTable = rule.myTable;
+            Tuple<int, int> moveAI = myAI.NextMove((side == 1) ? -1 : 1);
+            rule.myTable[moveAI.Item1, moveAI.Item2] = (side == 1) ? -1 : 1;
+            roundCount++;
+            if (rule.Referee(moveAI.Item1, moveAI.Item2, (side == 1) ? -1 : 1))
+            {
+                rule.ResetGame();
+                TempData["myTable"] = rule.myTable;
+                TempData["roundCount"] = 0;
+                return Json(new { yAI = moveAI.Item1, xAI = moveAI.Item2, sideAI = (side == 1) ? -1 : 1, flag = -99, message = "Computer wins." }, JsonRequestBehavior.AllowGet); // AI wins the game.
+            }
+            //
+            TempData["roundCount"] = roundCount;
+            TempData["myTable"] = rule.myTable;
+            return Json(new { yAI = moveAI.Item1, xAI = moveAI.Item2, sideAI = (side == 1) ? -1 : 1, flag = 0, message = "" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ WebApplication1/Helper/AI.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Web;
+
+namespace WebApplication1.Helper
+{
+    public class AI
+    {
+
+        private class MinScoreFirstComparer : IComparer<Move>
+        {
+            public int Compare(Move x, Move y)
+            {
+                return x.score - y.score;
+            }
+        }
+        private class MaxScoreFirstComparer : IComparer<Move>
+        {
+            public int Compare(Move x, Move y)
+            {
+                return y.score - x.score;
+            }
+        }
+        internal struct Move
+        {
+            internal int x;
+            internal int y;
+            internal int score;
+        };
+
+        public int[,] myTable;
+
+        private Dictionary<string, string[]> weightTable;
+
+        private const int depthLimit = 4;
+
+        public void LoadWeightTable()
+        {
+            var patterns = File.ReadAllLines(HttpContext.Current.Server.MapPath("~/Helper/JSW.dll"));
+            weightTable = patterns.Select(p => p.Split(' ')).ToDictionary(a => a[0]);
+        }
+
+
+        private int Evaluate(int y, int x, int c, int[,] testBoard)
+        {
+            int[,] Direct = new int[4, 2] { { 1, 0 }, { 1, 1 }, { 0, 1 }, { -1, 1 } }; //YX
+            int sum = 0;
+            char[] pattern = new char[9];
+            for (int I = 0; I < 4; I++)
+            {
+                for (int K = -4; K < 5; K++)
+                {
+                    int j = K + 4;
+                    if ((y + K * Direct[I, 0] >= 0) && (y + K * Direct[I, 0] < 15) && (x + K * Direct[I, 1] >= 0) && (x + K * Direct[I, 1] < 15))
+                    {
+                        switch (testBoard[(y + K * Direct[I, 0]), (x + K * Direct[I, 1])])
+                        {
+                            case 0:
+                                pattern[j] = '_';
+                                break;
+                            case 1:
+                                pattern[j] = 'w';
+                                break;
+                            case -1:
+                                pattern[j] = 'b';
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        pattern[j] = ((c == 1) ? 'b' : 'w');
+                    }
+
+                }
+                sum += int.Parse(weightTable[new string(pattern)][1]);
+            }
+            return sum;
+        }
+
+        private int MinMax(int[,] testBoard, int side, int depth, int alpha, int beta)
+        {
+            if (depth < depthLimit)
+            {
+                Move[] MyMove = new Move[256];
+                int index = 0;
+                for (int I = 0; I < 15; I++)
+                {
+                    for (int J = 0; J < 15; J++)
+                    {
+                        if (testBoard[J, I] == 0)
+                        {
+                            int selfScore = 0;
+                            int enemyScore = 0;
+                            testBoard[J, I] = side;
+                            selfScore = Evaluate(J, I, side, testBoard);  //yx
+                            testBoard[J, I] = (side == 1) ? -1 : 1;
+                            enemyScore = Evaluate(J, I, (side == 1) ? -1 : 1, testBoard);
+                            testBoard[J, I] = 0;
+                            MyMove[index].x = I;
+                            MyMove[index].y = J;
+                            MyMove[index].score = Math.Abs(selfScore) + Math.Abs(enemyScore);
+                            /**/
+                            if (Math.Abs(selfScore) >= Math.Abs(int.Parse(weightTable["____wwwww"][1])))
+                            {
+                                return (side == 1) ? 10000000 : -10000000;
+                            }
+                            /**/
+                            index++;
+                        }
+                    }
+                }
+                Array.Sort(MyMove, new MaxScoreFirstComparer());
+                int max = int.MinValue;
+                int min = int.MaxValue;
+                for (int I = 0; I < index && I < 10; I++)
+                {
+                    int selfScore = 0;
+                    testBoard[(MyMove[I].y), (MyMove[I].x)] = side;
+                    selfScore = MinMax(testBoard, (side == 1 ? -1 : 1), depth + 1, alpha, beta);
+                    testBoard[(MyMove[I].y), (MyMove[I].x)] = 0;
+                    if (side == 1)
+                    {
+                        if (selfScore > max)
+                        {
+                            max = selfScore;
+                            if (alpha < max)
+                            {
+                                alpha = max;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (selfScore < min)
+                        {
+                            min = selfScore;
+                            if (beta > min)
+                            {
+                                beta = min;
+                            }
+                        }
+                    }
+                    if (alpha >= beta)
+                    {
+                        return (side == 1 ? max : min);
+                    }
+                }
+                return (side == 1 ? max : min);
+            }
+            else
+            {
+                int sumOfScores = 0;
+                for (int I = 0; I < 15; I++)
+                {
+                    for (int J = 0; J < 15; J++)
+                    {
+                        if (testBoard[J, I] == 0)
+                        {
+                            int selfScore = 0;
+                            int enemyScore = 0;
+                            testBoard[J, I] = (side == 1) ? -1 : 1;
+                            enemyScore = Evaluate(J, I, (side == 1) ? -1 : 1, testBoard);
+                            sumOfScores += enemyScore;
+                            testBoard[J, I] = side;
+                            selfScore = Evaluate(J, I, side, testBoard);
+                            sumOfScores += selfScore;
+                            testBoard[J, I] = 0;
+                            /**/
+                            if (Math.Abs(selfScore) >= Math.Abs(int.Parse(weightTable["____wwwww"][1])))
+                            {
+                                return (side == 1) ? 10000000 : -10000000;
+                            }
+                            /**/
+                        }
+                    }
+                }
+                return sumOfScores;
+            }
+        }
+
+        private delegate int MinMaxFunctionDelegate(int[,] testBoard, int side, int depth, int alpha, int beta);
+
+        public Tuple<int, int> NextMove(int side)
+        {
+            Move[] myMove = new Move[256];
+            int index = 0;
+            for (int I = 0; I < 15; I++)
+            {
+                for (int J = 0; J < 15; J++)
+                {
+                    if (myTable[J, I] == 0)
+                    {
+                        int selfScore = 0;
+                        int enemyScore = 0;
+                        myTable[J, I] = side;
+                        selfScore = Evaluate(J, I, side, myTable);  //yx
+                        myTable[J, I] = (side == 1) ? -1 : 1;
+                        enemyScore = Evaluate(J, I, (side == 1) ? -1 : 1, myTable);
+                        myTable[J, I] = 0;
+                        myMove[index].x = I;
+                        myMove[index].y = J;
+                        myMove[index].score = Math.Abs(selfScore) + Math.Abs(enemyScore);
+                        if (Math.Abs(selfScore) >= int.Parse(weightTable["____wwwww"][1]))
+                        {
+                            return new Tuple<int, int>(myMove[index].y, myMove[index].x);
+                        }
+                        index++;
+                    }
+                }
+            }
+            Array.Sort(myMove, (side == 1) ? (IComparer<Move>)(new MaxScoreFirstComparer()) : (IComparer<Move>)(new MinScoreFirstComparer()));
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            MinMaxFunctionDelegate minMaxFunctionDelegate = new MinMaxFunctionDelegate(MinMax);
+            List<KeyValuePair<int,IAsyncResult>> asyncResultList = new List<KeyValuePair<int,IAsyncResult>>();
+            for (int I = 0; I < index && I < 10; I++)
+            {
+                int[,] NextBoard = (int[,])myTable.Clone();
+                NextBoard[(myMove[I].y), (myMove[I].x)] = side;
+                asyncResultList.Add(new KeyValuePair<int, IAsyncResult>(I, minMaxFunctionDelegate.BeginInvoke(NextBoard, (side == 1 ? -1 : 1), 0, max, min, null, null)));
+                if ((I+1) % (Environment.ProcessorCount) == 0)
+                {
+                    WaitAllThreads(asyncResultList, minMaxFunctionDelegate, ref myMove, side, ref max, ref min);
+                }
+            }
+            if (asyncResultList.Any())
+            {
+                WaitAllThreads(asyncResultList, minMaxFunctionDelegate, ref myMove, side, ref max, ref min);
+            }
+            Array.Sort(myMove, 0, 10, (side == 1) ? (IComparer<Move>)(new MaxScoreFirstComparer()) : (IComparer<Move>)(new MinScoreFirstComparer()));
+            return new Tuple<int, int>(myMove[0].y, myMove[0].x);
+        }
+
+        private void WaitAllThreads(List<KeyValuePair<int, IAsyncResult>> asyncResultList, MinMaxFunctionDelegate minMaxFunctionDelegate, ref Move[] myMove, int side, ref int max, ref int min)
+        {
+            WaitHandle.WaitAll(asyncResultList.Select(r => r.Value.AsyncWaitHandle).ToArray());
+            foreach (var result in asyncResultList)
+            {
+                int selfScore = minMaxFunctionDelegate.EndInvoke(result.Value);
+                myMove[result.Key].score = selfScore;
+                if (side == 1)
+                {
+                    if (selfScore > max)
+                    {
+                        max = selfScore;
+                    }
+                }
+                else
+                {
+                    if (selfScore < min)
+                    {
+                        min = selfScore;
+                    }
+                }
+            }
+            asyncResultList.Clear();
+        }
+    }
+}
